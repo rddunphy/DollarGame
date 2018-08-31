6444ba1786b954bdaa9932d977cdf615d769841c
R. David Dunphy <rdd.dunphy@gmail.com>
2018-08-31 22:13:00 +0100

    Add graph class

--- js/graph.js
+++ js/graph.js
@@ -1,6 +1,11 @@
+function Graph() {
+    this.nodes = []
+    this.add_node = function(value) {
+        this.nodes.push(new Node(value));
+    };
+}
 
-function Node(id, value) {
-    this.id = id;
+function Node(value) {
     this.value = value;
     this.connected = [];
     this.add_edge = function(node) {
@@ -8,17 +13,17 @@ function Node(id, value) {
             this.connected.push(node);
             node.connected.push(this);
         }
-    }
+    };
     this.give = function() {
         for (var i=0; i < this.connected.length; i++) {
             this.value -= 1;
             this.connected[i].value += 1;
         }
-    }
+    };
     this.take = function() {
         for (var i=0; i < this.connected.length; i++) {
             this.connected[i].value -= 1;
             this.value += 1;
         }
-    }
+    };
 }
