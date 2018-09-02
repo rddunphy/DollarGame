4e9504e17e6b2af4b4a5d01e8880ae1bd352f093
R. David Dunphy <rdd.dunphy@gmail.com>
2018-09-02 22:35:50 +0100

    Add graph nodes.

---
+++ js/draw.js
@@ -0,0 +1,76 @@
+var graphAreaDiv = document.getElementById("graph_area");
+
+var StateEnum = Object.freeze({
+    "ADD_NODE": 0,
+    "ADD_EDGE_A": 1,
+    "ADD_EDGE_B": 2,
+    "NODE_GIVE": 3,
+    "NODE_TAKE": 4
+});
+var graph = new Graph();
+var state = StateEnum.ADD_NODE;
+var edgeNodeA = undefined;
+
+function handleNodeClick(nodeId) {
+    console.log(state);
+    var node = graph.nodes[nodeId];
+    if (state == StateEnum.NODE_GIVE) {
+        node.give();
+    } else if (state == StateEnum.NODE_TAKE) {
+        node.take();
+    } else if (state == StateEnum.ADD_EDGE_A) {
+        edgeNodeA = nodeId;
+        state = StateEnum.ADD_EDGE_B;
+    } else if (state == StateEnum.ADD_EDGE_B) {
+        if (edgeNodeA != nodeId) {
+            var nodeA = graph.nodes[edgeNodeA];
+            var nodeB = graph.nodes[nodeId];
+            nodeA.addEdge(nodeB);
+            state = StateEnum.ADD_EDGE_A;
+        }
+    }
+}
+
+function createNode(x, y) {
+    x -= 15;
+    y -= 15;
+    var nodeDiv = document.createElement('div');
+    nodeDiv.classList.add("node");
+    nodeDiv.style.position = "absolute";
+    nodeDiv.style.left = x + "px";
+    nodeDiv.style.top = y + "px";
+    var id = graph.addNode(nodeDiv);
+    nodeDiv.onclick = function() {
+        handleNodeClick(id);
+    };
+    graphAreaDiv.appendChild(nodeDiv);
+}
+
+function handleGraphAreaClick(x, y) {
+    console.log("graph_area click at (" + x + ", " + y + ").")
+    if (state == StateEnum.ADD_NODE) {
+        createNode(x, y);
+    }
+}
+
+$("#graph_area").click(function(e) {
+    if (e.target == this) {
+        handleGraphAreaClick(e.pageX, e.pageY);
+    }
+});
+
+$("#add_node_btn").click(function(e) {
+    state = StateEnum.ADD_NODE;
+});
+
+$("#add_edge_btn").click(function(e) {
+    state = StateEnum.ADD_EDGE_A;
+});
+
+$("#node_give_btn").click(function(e) {
+    state = StateEnum.NODE_GIVE;
+});
+
+$("#node_take_btn").click(function(e) {
+    state = StateEnum.NODE_TAKE;
+});
--- js/graph.js
+++ js/graph.js
@@ -1,14 +1,29 @@
+
+function newNodeId(nodes) {
+    for (var i = 0; i < Object.entries(nodes).length + 1; i++) {
+        if (!(i in nodes)) {
+            return i;
+        }
+    }
+}
+
 function Graph() {
-    this.nodes = []
-    this.add_node = function(value) {
-        this.nodes.push(new Node(value));
+    this.nodes = new Object();
+    this.addNode = function(div) {
+        var id = newNodeId(this.nodes);
+        this.nodes[id] = new Node(div, 0);
+        return id;
     };
 }
 
-function Node(value) {
-    this.value = value;
+function Node(div, value) {
+    this.div = div;
     this.connected = [];
-    this.add_edge = function(node) {
+    this.setValue = function(value) {
+        this.value = value;
+        this.div.innerHTML = "<span>" + value + "</span>";
+    };
+    this.addEdge = function(node) {
         if (!this.connected.includes(node)) {
             this.connected.push(node);
             node.connected.push(this);
@@ -16,14 +31,15 @@ function Node(value) {
     };
     this.give = function() {
         for (var i=0; i < this.connected.length; i++) {
-            this.value -= 1;
-            this.connected[i].value += 1;
+            this.connected[i].setValue(this.connected[i].value + 1);
         }
+        this.setValue(this.value - this.connected.length);
     };
     this.take = function() {
         for (var i=0; i < this.connected.length; i++) {
-            this.connected[i].value -= 1;
-            this.value += 1;
+            this.connected[i].setValue(this.connected[i].value - 1);
         }
+        this.setValue(this.value + this.connected.length);
     };
+    this.setValue(value);
 }
